793fea8f5fe50f03bfb4107ad8f2695c2c6516c2
Alisson Santos <alisson.wiin@gmail.com>
2026-03-03 22:46:49 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/use-cases/add-account/db-add-account-protocols.ts
+++ src/data/use-cases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export type * from '../../../domain/use-cases/add-account'
 export type * from '../../../domain/use-cases/models/account'
 export type * from '../../protocols/encrypter'
+export type * from '../../protocols/add-account-repository'
