c1b9f85168d63bab8673291ac9dc08c1e5c014ea
Alisson Santos <alisson.wiin@gmail.com>
2026-03-04 22:28:11 -0300

    test: add unit test to verify BcryptAdapter returns hashed value on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,11 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  hash: async (_value: string, _salt: number): Promise<string> =>
+    await Promise.resolve('hashed_value'),
+}))
+
 describe('Bcrypt Adapter', () => {
   it('should call bcrypt with correct values', async () => {
     const salt = 12
@@ -8,5 +13,10 @@ describe('Bcrypt Adapter', () => {
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+  it('should return a hash on success', async () => {
+    const sut = new BcryptAdapter(12)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hashed_value')
   })
 })
