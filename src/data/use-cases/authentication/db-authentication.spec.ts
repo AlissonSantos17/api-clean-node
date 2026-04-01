a7bb5375b5d3b7f7f06fc3f335fd6e28ade43724
Alisson Santos <alisson.wiin@gmail.com>
2026-03-31 23:06:49 -0300

    chore: ensure DbAuthentication returns null

--- src/data/use-cases/authentication/db-authentication.spec.ts
+++ src/data/use-cases/authentication/db-authentication.spec.ts
@@ -49,4 +49,11 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  it('should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(Promise.resolve(null))
+    const authentication = await sut.auth(makeFakeAuthentication())
+    expect(authentication).toBeNull()
+  })
 })
