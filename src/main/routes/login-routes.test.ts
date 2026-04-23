b6417de8ceeb1f788e6596a28bbc6c997c0193b2
Alisson Santos <alisson.wiin@gmail.com>
2026-04-22 23:11:02 -0300

    test: login route returns 301 with invalid credentials

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -50,5 +50,15 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('should return 401 on login', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'alissonfelp00@gmail.com',
+          password: '123456',
+        })
+        .expect(401)
+    })
   })
 })
