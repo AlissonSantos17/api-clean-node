b53800910b62fbfaed3281ee1fea25b247ed08b9
Alisson Santos <alisson.wiin@gmail.com>
2026-05-10 20:51:25 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.protocols.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.protocols.ts
@@ -1,2 +1,2 @@
 export type * from '../../../protocols'
-export type * from '../../../../domain/models/authentication'
+export type * from '../../../../domain/models/add-survey'
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,14 +1,24 @@
-import type { Controller, HttpRequest, Validation } from './add-survey-controller.protocols'
 import { badRequest } from '../../../helpers/http/http-helper'
+import type {
+  AddSurvey,
+  Controller,
+  HttpRequest,
+  Validation,
+} from './add-survey-controller.protocols'
 
 export class AddSurveyController implements Controller {
-  constructor(private readonly validation: Validation) {}
+  constructor(
+    private readonly validation: Validation,
+    private readonly addSurvey: AddSurvey,
+  ) {}
 
   async handle(httpRequest: HttpRequest): Promise<any> {
     const error = this.validation.validate(httpRequest.body)
     if (error) {
       return badRequest(error)
     }
-    return await Promise.resolve(null)
+    const { question, answers } = httpRequest.body
+    await this.addSurvey.add({ question, answers })
+    return null
   }
 }
