37501d1204c1a9a6b28bb370780ce8862950aa60
Alisson Santos <alisson.wiin@gmail.com>
2026-06-08 21:55:02 -0300

    feat: ensure DbLoadAccountByToken call LoadAccountByTokenRepository with correct values

--- src/data/use-cases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/use-cases/load-account-by-token/db-load-account-by-token.ts
@@ -1,10 +1,17 @@
 import type { Decrypter } from '../../protocols/criptography/decrypter'
+import type { LoadAccountByTokenRepository } from '../../protocols/db/account/load-account-by-token-repository'
 
 export class DbLoadAccountByToken {
-  constructor(private readonly decrypter: Decrypter) {}
+  constructor(
+    private readonly decrypter: Decrypter,
+    private readonly loadAccountByTokenRepository: LoadAccountByTokenRepository,
+  ) {}
 
-  async load(accessToken: string, _role?: string): Promise<string | null> {
-    await this.decrypter.decrypt(accessToken)
-    return await Promise.resolve(null)
+  async load(accessToken: string, role?: string): Promise<string | null> {
+    const decryptedToken = await this.decrypter.decrypt(accessToken)
+    if (decryptedToken) {
+      await this.loadAccountByTokenRepository.loadByToken(decryptedToken, role)
+    }
+    return null
   }
 }
