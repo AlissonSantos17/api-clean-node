85fa754d4388f7862703bf8bd47153659aad0423
Alisson Santos <alisson.wiin@gmail.com>
2026-06-08 21:44:10 -0300

    refactor: move sut creation to a factory method

---
+++ src/data/use-cases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -0,0 +1,34 @@
+import type { Decrypter } from '../../protocols/criptography/decrypter'
+import { DbLoadAccountByToken } from './db-load-account-by-token'
+
+interface SutTypes {
+  sut: DbLoadAccountByToken
+  decryptStub: Decrypter
+}
+
+const makeDecrypter = (): Decrypter => {
+  class DecrypterStub implements Decrypter {
+    async decrypt(_value: string): Promise<string | null> {
+      return await Promise.resolve('any_value')
+    }
+  }
+  return new DecrypterStub()
+}
+
+const makeSut = (): SutTypes => {
+  const decryptStub = makeDecrypter()
+  const sut = new DbLoadAccountByToken(decryptStub)
+  return {
+    sut,
+    decryptStub,
+  }
+}
+
+describe('DbLoadAccountByToken UseCase', () => {
+  it('should call decrypter with correct values', async () => {
+    const { sut, decryptStub } = makeSut()
+    const decryptSpy = jest.spyOn(decryptStub, 'decrypt')
+    await sut.load('any_token')
+    expect(decryptSpy).toHaveBeenCalledWith('any_token')
+  })
+})
