3281df0f14e4f23ca1f9ce58800da035cfd492dc
Alisson Santos <alisson.wiin@gmail.com>
2026-06-15 22:02:38 -0300

    test: refactor and organize unit tests for BcryptAdapter and AccountMongoRepository

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -20,51 +20,57 @@ describe('Account Mongo Repository', () => {
 
   const makeSut = (): AccountMongoRepository => new AccountMongoRepository()
 
-  it('should return an account on add success', async () => {
-    const sut = makeSut()
-    const account = await sut.add({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
+  describe('add()', () => {
+    it('should return an account on add success', async () => {
+      const sut = makeSut()
+      const account = await sut.add({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      })
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@mail.com')
-    expect(account.password).toBe('any_password')
   })
 
-  it('should return an account on loadByEmail success', async () => {
-    const sut = makeSut()
-    await accountsCollection.insertOne({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
+  describe('loadByEmail()', () => {
+    it('should return an account on loadByEmail success', async () => {
+      const sut = makeSut()
+      await accountsCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      })
+      const account = await sut.loadByEmail('any_email@mail.com')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
     })
-    const account = await sut.loadByEmail('any_email@mail.com')
-    expect(account).toBeTruthy()
-    expect(account.id).toBeTruthy()
-    expect(account.name).toBe('any_name')
-    expect(account.email).toBe('any_email@mail.com')
-    expect(account.password).toBe('any_password')
+
+    it('should return null if loadByEmail fails', async () => {
+      const sut = makeSut()
+      const account = await sut.loadByEmail('invalid_email@mail.com')
+      expect(account).toBeFalsy()
+    })
   })
 
-  it('should return null if loadByEmail fails', async () => {
-    const sut = makeSut()
-    const account = await sut.loadByEmail('invalid_email@mail.com')
-    expect(account).toBeFalsy()
-  })
-
-  it('should update the account accessToken on updateAccessToken success', async () => {
-    const sut = makeSut()
-    const fakeAccount = await accountsCollection.insertOne({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
+  describe('updateAccessToken()', () => {
+    it('should update the account accessToken on updateAccessToken success', async () => {
+      const sut = makeSut()
+      const fakeAccount = await accountsCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      })
+      await sut.updateAccessToken(fakeAccount.insertedId.toString(), 'any_token')
+      const account = await accountsCollection.findOne({ _id: fakeAccount.insertedId })
+      expect(account).toBeTruthy()
+      expect(account?.accessToken).toBe('any_token')
     })
-    await sut.updateAccessToken(fakeAccount.insertedId.toString(), 'any_token')
-    const account = await accountsCollection.findOne({ _id: fakeAccount.insertedId })
-    expect(account).toBeTruthy()
-    expect(account?.accessToken).toBe('any_token')
   })
 })
