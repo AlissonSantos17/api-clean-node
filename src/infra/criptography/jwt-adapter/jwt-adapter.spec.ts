48b091960c118bf8dda8c592d189571ca8d5d017
Alisson Santos <alisson.wiin@gmail.com>
2026-06-15 22:15:22 -0300

    test: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -47,5 +47,15 @@ describe('Jwt Adapter', () => {
       const value = await sut.decrypt('any_token')
       expect(value).toBe('any_value')
     })
+
+    it('should throw if verify throws', async () => {
+      const sut = makeSut()
+      const throwError = (): never => {
+        throw new Error()
+      }
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(throwError)
+      const promise = sut.decrypt('any_token')
+      await expect(promise).rejects.toThrow()
+    })
   })
 })
